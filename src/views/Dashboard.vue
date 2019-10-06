6e2f06c6dc3d4ac047789a1d3c65a4e73f0763f8
Pazekal90 <mail@pascal-pischel.de>
2019-10-06 23:36:34 +0200

    Testing Cols

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,15 +1,20 @@
 <template>
-    <v-container fluid>
-        <v-row>
+    <v-container>
+        <v-row justify="center">
             <v-col xs="12" class="text-xs-center display-1 font-weight-black">Dashboard</v-col>
         </v-row>
-        <v-row>
-            <v-col xs="12" lg="4">
+        <v-row justify="center">
+            <v-col cols="12" xs="12" sm="6" lg="4">
                 <v-card class="pa-2" tile outlined>
                     col
                 </v-card>
             </v-col>
-            <v-col xs="12" lg="4">
+            <v-col cols="12" xs="12" sm="6" lg="4">
+                <v-card class="pa-2" tile outlined>
+                    col
+                </v-card>
+            </v-col>
+            <v-col cols="12" xs="12" sm="12" lg="4">
                 <v-card class="pa-2" tile outlined>
                     col
                 </v-card>
