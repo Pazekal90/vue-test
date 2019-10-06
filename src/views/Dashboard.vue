f5ad4694cc2f7b515d43cd09f0107601be4e3f64
Pazekal90 <mail@pascal-pischel.de>
2019-10-06 23:19:40 +0200

    Col Tests in Dashboard and updated Dependencies

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,20 @@
 <template>
-    <v-container>
-        <h2>Dashboard</h2>
+    <v-container fluid>
+        <v-row>
+            <v-col xs="12" class="text-xs-center display-1 font-weight-black">Dashboard</v-col>
+        </v-row>
+        <v-row>
+            <v-col xs="12" lg="4">
+                <v-card class="pa-2" tile outlined>
+                    col
+                </v-card>
+            </v-col>
+            <v-col xs="12" lg="4">
+                <v-card class="pa-2" tile outlined>
+                    col
+                </v-card>
+            </v-col>
+        </v-row>
     </v-container>
 
 </template>
