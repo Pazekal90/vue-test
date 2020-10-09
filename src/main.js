de9eaf864a5fb66be3ac24d6a92440447d299ebd
Pazekal90 <mail@pascal-pischel.de>
2020-10-09 17:30:41 +0200

    Upgraded all Dependencies

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify';
 
 Vue.use(Vuetify)
 
@@ -10,6 +11,7 @@ Vue.config.productionTip = false
 const app = new Vue({
   router,
   Vuetify,
+  vuetify,
   render: h => h(App)
 }).$mount('#app');
 if (window.Cypress) {
