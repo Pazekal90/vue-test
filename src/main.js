4a70697125a8e3c34287d933b92840911423622e
Pascal Pischel <mail@pascal-pischel.de>
2020-10-09 17:31:56 +0200

    Fixedvulnerabilities (#35)
    * Change Readme Greenkeeper no longer maintained
    
    * Fixed Vulnerabilities
    
    * Upgraded all Dependencies
    
    * Upgraded all Dependencies

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
 const app = new Vue({
   router,
+  Vuetify,
   vuetify,
   render: h => h(App)
 }).$mount('#app');
